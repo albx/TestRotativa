4986ab0aa47128d7a553de85c8d82198ab2d8084
Alberto Mori <albi.mo87@hotmail.it>
2018-10-09 11:31:35 +0200

    Aggiungere i file di progetto.

---
+++ TestRotativa/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestRotativa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
